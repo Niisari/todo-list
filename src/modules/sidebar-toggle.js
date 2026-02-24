5844f096e68194cdc10baf42ee10fcb26ae14968
Yushi <yushari@seznam.cz>
2026-02-24 11:44:17 +0100

    added close, active logic to all the buttons

--- src/modules/sidebar-toggle.js
+++ src/modules/sidebar-toggle.js
@@ -1,9 +1,21 @@
 export const sidebarToggle = () => {
-    const sidebar = document.getElementById("todo-sidebar");
-    const sidebarBtn = document.querySelectorAll(".hide__wheel");
-        sidebarBtn.forEach(btn => {
-                btn.addEventListener("click", () => {
-                    sidebar.classList.toggle("active");
-                });
-            });
+    const sidebar = document.getElementById('todo-sidebar');
+    const toggleBtns = document.querySelectorAll('.logo__btn, .hide__wheel');
+    
+    // toggle logic for the menu buttons
+    toggleBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            sidebar.classList.toggle('active'); 
+        });
+    });
+
+    // Select all your main navigation buttons
+    const navButtons = document.querySelectorAll('.inbox__btn, .today__btn, .tomorrow__btn, .week__btn, .all__btn');
+
+    navButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            // Remove the active class to slide the sidebar away
+            sidebar.classList.remove('active'); 
+        });
+    });
 };
