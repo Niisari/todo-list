a6144cc1d4153217fe1f82ccc3bdac34a32439f2
Yushi <yushari@seznam.cz>
2026-02-25 09:47:47 +0100

    Removed ID as it's not needed

--- src/modules/project.js
+++ src/modules/project.js
@@ -58,7 +58,6 @@ const showProjectForm = (container) => {
 const createProjectElement = (name, container) => {
     const projectBtn = document.createElement('button');
     projectBtn.className = 'project__item--btn';
-    projectBtn.id = 'project-btn';
     projectBtn.innerHTML = `
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--Buttons-Icons)" stroke-width="2">
             <path d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
